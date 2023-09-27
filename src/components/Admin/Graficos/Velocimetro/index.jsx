d3e23adffd08996bb725d952321accd05d4f7cbe
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-09-27 14:46:08 -0300

    Update
    Aplicação completa.. Hora de apresentar para o chefe.

--- src/components/Admin/Graficos/Velocimetro/index.jsx
+++ src/components/Admin/Graficos/Velocimetro/index.jsx
@@ -1,10 +1,14 @@
 "use client"
-import React,{useState, useEffect} from 'react'
-import {collection, getDocs, orderBy, query, where} from 'firebase/firestore'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore'
 import { db } from '@/utils/firebase'
 import styles from './Velocimetro.module.css'
 //import {Chart as ChartJS, CategoryScale, LinearScale,BarElement, Title, Tooltip, Legend} from 'chart.js'
 //import {Bar} from "react-chartjs-2"
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ weight: '700', subsets: ['latin'], style: 'normal' })
 
 
 export default function Velocimetro() {
@@ -18,7 +22,7 @@ export default function Velocimetro() {
             const snapDataQuery = []
 
             snapQuery.forEach((doc) => {
-                snapDataQuery.push({id: doc.id, ...doc.data()})
+                snapDataQuery.push({ id: doc.id, ...doc.data() })
             })
             setGraph(snapDataQuery)
         }
@@ -32,8 +36,8 @@ export default function Velocimetro() {
     var somaValor = value.reduce((accumulator, currentVelue) => accumulator + currentVelue, 0)
     var valorMaximo = 500000
     const porcentagemPreenchimento = (somaValor / valorMaximo) * 100 * 10
-    const strokeDasharray = `${porcentagemPreenchimento} ${1000 - porcentagemPreenchimento}` 
-    
+    const strokeDasharray = `${porcentagemPreenchimento} ${1000 - porcentagemPreenchimento}`
+
 
 
     return (
@@ -41,7 +45,7 @@ export default function Velocimetro() {
             <div
                 className={`flex flex-col justify-center items-center`}
             >
-                <p className={styles.titulo}>Valor em R$</p>
+                <p className={`${styles.titulo} ${inter.className}`}>Montante Pago em R$</p>
                 <svg width={300} height={300}>
                     <circle
                         cx={150}
@@ -55,18 +59,19 @@ export default function Velocimetro() {
                         cx={150}
                         cy={150}
                         r={130}
-                        stroke="rgba(255, 159, 64, 1)"
+                        stroke="#2ecc71"
                         fill="transparent"
                         strokeWidth={30}
                         fillOpacity={porcentagemPreenchimento / 100}
                         strokeDasharray={strokeDasharray}
                     />
+                    {/* rgba(255, 159, 64, 1) */}
                 </svg>
-            <div className={styles.somaValor}>
-                <p>R$ {somaValor.toFixed(2).replace('.', ',')}</p>
+                <div className={styles.somaValor}>
+                    <p>R$ {somaValor.toFixed(2).replace('.', ',')}</p>
+                </div>
             </div>
-            </div>
-            
+
         </>
     )
 }
